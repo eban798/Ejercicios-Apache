4b1748885d0faad809d0c911e37ea1b4c48da379
eban798 <108815655+eban798@users.noreply.github.com>
2022-10-12 11:35:55 -0300

    Ejercicio1.2

---
+++ Ejercicio1.2/src/ejercicio1/pkg2/Ejercicio12.java
@@ -0,0 +1,24 @@
+package ejercicio1.pkg2;
+
+/**
+ *
+ * @author PC 21
+ */
+import java.util.Scanner;
+
+public class Ejercicio12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int edad;
+        Scanner sc = new Scanner(System.in);
+        edad = sc.nextInt();
+        edad++;
+        System.out.println("El proximo año su edad sera: " + edad +" años");
+        // TODO code application logic here
+    }
+    
+}
